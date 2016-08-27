83f5cc0b8ef00be0bb558d4aaded0567975122fd
Dalton386 <18069820300@163.com>
2016-08-28 04:28:11 +0800

    optimization of database

--- database.sql
+++ database.sql
@@ -18,6 +18,7 @@ CREATE TABLE IF NOT EXISTS STATUS_ROUND(
 
 CREATE TABLE IF NOT EXISTS interviewees(
 	studentid varchar(20) NOT NULL PRIMARY KEY,
+    studentname char(15) not null,
 	serialnumber varchar(50) NOT NULL,
 	serialdigit varchar(10) NOT NULL,
 	status int NOT NULL,
@@ -34,8 +35,8 @@ CREATE TABLE IF NOT EXISTS rates(
 	serialdigit  varchar(10) NOT NULL,
 	uid int NOT NULL,
 	round int NOT NULL,
-	sector int NOT NULL,
-	rating int NOT NULL
+	t_rates int NOT NULL,
+	r_rates int NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS comments(
@@ -49,58 +50,36 @@ CREATE TABLE IF NOT EXISTS comments(
 	comment varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS tags(
-	tagid int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	studentid varchar(20) NOT NULL,
-	serialnumber varchar(255) NOT NULL,
-	serialdigit  varchar(10) NOT NULL,
-	uid int NOT NULL,
-	round int NOT NULL,
-	sector int NOT NULL,
-	tags int NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS tags_meta(
-	tagid int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  description varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS rooms(
 	rid int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  roomnumber varchar(10) NOT NULL,
+  	roomnumber varchar(10) NOT NULL,
 	roomstatus int NOT NULL,
-	sector int NOT NULL,
 	gid int NOT NULL,
 	susr_count int NOT NULL,
 	suid_core int NOT NULL,
 	suid1 int NOT NULL,
 	suid2 int NOT NULL,
 	suid3 int NOT NULL,
-	suid4 int NOT NULL,
-	suid5 int NOT NULL,
-	suid6 int NOT NULL,
-	suid7 int NOT NULL,
-	suid8 int NOT NULL,
-	suid9 int NOT NULL,
-	suid10 int NOT NULL
+	suid4 int NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS groups(
 	gid int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	round int NOT NULL,
+	status int NOT NULL,
 	member_count int NOT NULL,
-	member1_sn varchar(255) NOT NULL,
-	member2_sn varchar(255) NOT NULL,
-	member3_sn varchar(255) NOT NULL,
-	member4_sn varchar(255) NOT NULL,
-	member5_sn varchar(255) NOT NULL,
-	member6_sn varchar(255) NOT NULL,
-	member7_sn varchar(255) NOT NULL,
-	member8_sn varchar(255) NOT NULL,
-  member9_sn varchar(255) NOT NULL,
-  member10_sn varchar(255) NOT NULL,
-  member11_sn varchar(255) NOT NULL,
-  member12_sn varchar(255) NOT NULL
+	member1_sn varchar(255) default 0 NOT NULL,
+	member2_sn varchar(255) default 0 NOT NULL,
+	member3_sn varchar(255) default 0 NOT NULL,
+	member4_sn varchar(255) default 0 NOT NULL,
+	member5_sn varchar(255) default 0 NOT NULL,
+	member6_sn varchar(255) default 0 NOT NULL,
+	member7_sn varchar(255) default 0 NOT NULL,
+	member8_sn varchar(255) default 0 NOT NULL,
+  	member9_sn varchar(255) default 0 NOT NULL,
+  	member10_sn varchar(255) default 0 NOT NULL,
+  	member11_sn varchar(255) default 0 NOT NULL,
+  	member12_sn varchar(255) default 0 NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS times(
@@ -110,37 +89,16 @@ CREATE TABLE IF NOT EXISTS times(
 	description varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS sectors(
-	sid int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	description varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  naming varchar(25)
-);
-
-CREATE TABLE IF NOT EXISTS television_waiting_list(
+CREATE TABLE IF NOT EXISTS waiting_list(
 	studentid varchar(20) NOT NULL,
+    studentname char(15) not null,
 	serialnumber varchar(255) NOT NULL PRIMARY KEY,
 	serialdigit  varchar(10) NOT NULL,
 	inqueuetime varchar(100) NOT NULL,
-	status int NOT NULL
+	sector int NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS radio_waiting_list(
-	studentid varchar(20) NOT NULL,
-	serialnumber varchar(255) NOT NULL PRIMARY KEY,
-	serialdigit  varchar(10) NOT NULL,
-	inqueuetime varchar(100) NOT NULL,
-	status int NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS newspaper_waiting_list(
-	studentid varchar(20) NOT NULL,
-	serialnumber varchar(255) NOT NULL PRIMARY KEY,
-	serialdigit  varchar(10) NOT NULL,
-	inqueuetime varchar(100) NOT NULL,
-  status int NOT NULL
-);
-
-create table if not exists simu_enroll(
+create table if not exists enroll2016_application(
         studentid char(10) not null,
         studentname char(15) not null,
         mobilephone char(11) not null,
@@ -164,5 +122,5 @@ create table if not exists simu_enroll(
         submittime datetime default null,
         version int default 0 not null,
         resultstatus int default 1 not null,
-        primary key (studentId,version)
+        primary key (studentid,version)
 )ENGINE=MyISAM DEFAULT CHARSET=utf8;
